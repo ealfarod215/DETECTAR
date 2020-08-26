b3b1a5ce3dc142e6c3a39d55be45ec06e31c6e1f
ealfarod215 <ealfarod215@ulacit.ed.cr>
2020-08-26 17:16:03 -0600

    añadiendo proyecto a git

---
+++ Logica.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deteccion
+{
+    class Logica
+    {
+       public void DETECTAR()
+       {
+            string palabra;
+            string letra;
+            var comparador = StringComparer.InvariantCultureIgnoreCase;
+            var cuenta = 0;
+
+            Console.WriteLine("Por favor digite una palabra:");
+            while ((palabra = Console.ReadLine()) != null)
+            {
+                var conjunto = new HashSet<string>(new string[] { }, comparador);
+                for (int x = 0; x < palabra.Length; x++)
+                {
+                    letra = palabra[x].ToString();
+                    if (conjunto.Add(letra))
+                    {
+                        int contador = 1;
+                        for (int y = x + 1; y < palabra.Length; y++)
+                        {
+                            if (comparador.Equals(letra, palabra[y].ToString()))
+                            {
+                                contador++;
+                                cuenta = contador;
+                            }
+                        }
+                    }
+                }
+                if (cuenta >= 2)
+                {
+                    Console.WriteLine("Verdadero");
+                    DETECTAR();
+                }
+                else
+                {
+                    Console.WriteLine("Falso");
+                    DETECTAR();
+                }
+            }
+       }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Deteccion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Logica logica = new Logica();
+
+            logica.DETECTAR();
+
+        }
+
+        
+
+
+
+
+    }
+}
